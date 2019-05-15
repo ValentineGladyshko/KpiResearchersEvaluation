4154b9654bbafd38cce8de167d3fbc0def93cc91
ValentineGladyshko <valentine9022@gmail.com>
2019-05-15 13:11:39 +0300

    Added new  functionality

---
+++ WebLibrary/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KpiResearchersEvaluation.WebLibrary
+{
+    public static class WebSocket
+    {
+        private static readonly HttpClient client;
+
+        static WebSocket()
+        {
+            ServicePointManager.DefaultConnectionLimit = 10;
+            client = new HttpClient();
+        }
+
+        public static async Task<string> GetPage(string url)
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch (HttpRequestException e)
+            {
+                return string.Empty;
+            }
+
+        }
+    }
+}
